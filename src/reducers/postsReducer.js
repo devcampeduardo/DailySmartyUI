e3075a06b122d1d6cda88f1cd258ed2b43983f45
devcampeduardo <edersanchez6467@gmail.com>
2021-12-11 05:59:41 -0600

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,20 +1,28 @@
 import{
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
-import recentPost from '../components/recentPost';
+
 
 const INIT_STATE ={
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
 export default function(state= INIT_STATE, action) {
     switch(action.type) {
         case SET_RECENT_POSTS:
-        const recentPosts= action.playload;
+            const recentPosts= action.playload;
+            return {
+                ...state, 
+                recentPosts
+            }
+
+    case SET_RESULTS_POSTS:
+        const resultsPosts = action.playload;
         return {
-            ...state, 
-            recentPosts
+            ...state,
+            resultslPosts
         }
         default:
             return state;
