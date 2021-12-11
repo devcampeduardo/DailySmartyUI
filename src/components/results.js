6afc6127ba2dc5bb4a6b73cc568826e2d6a60dd7
devcampeduardo <edersanchez6467@gmail.com>
2021-12-11 04:40:43 -0600

    put together resultsposts component

--- src/components/results.js
+++ src/components/results.js
@@ -3,6 +3,7 @@ import React, { Component } from "react"
 
 import Logo from "./logo";
 import SearchBar from "./searchBar";
+import ResultsPosts from "./resultsPosts";
 
 import {connect} from "react-redux";
 import * as actions from "../actions";
@@ -18,6 +19,7 @@ class Results extends Component {
             <div>
                 <Logo size={55}/>
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <ResultsPosts/>
             </div>
         )
     }
